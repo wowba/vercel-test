aaf38d3b4b91358689dd65fa4582abfa5c2e3497
wowba <leeyuwk54@gmail.com>
2023-12-14 12:27:27 +0900

    vercel middleware add console

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ export default function middleware(request: Request) {
   const url = new URL(request.url);
 
   if (url.pathname.startsWith('/api')) {
+    console.log(new URL(process.env.BACKEND_ENDPOINT, request.url));
     return rewrite(new URL(process.env.BACKEND_ENDPOINT, request.url));
   }
 }
