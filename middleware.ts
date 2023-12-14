481407787fd939a9d7b134d14eba44f703e10aa2
wowba <leeyuwk54@gmail.com>
2023-12-15 06:35:24 +0900

    github action 테스트

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { rewrite } from '@vercel/edge';
-
-export default function middleware(request: Request) {
-  const url = new URL(request.url);
-
-  if (url.pathname.startsWith('/api')) {
-    return rewrite(new URL(process.env.BACKEND_ENDPOINT, request.url));
-  }
-}
